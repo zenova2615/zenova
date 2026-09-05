17bb400c6420295d60e787943bd01b4a03c4d831
zenova2615 <shafiahmedmele@gmail.com>
2026-09-05 12:23:43 +0600

    Add AppConstants class with application settings
    Define application constants for ZENOVA.

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,26 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String appName = 'ZENOVA';
+  static const String appFullName = 'ZENOVA Media';
+  static const String appTagline = 'Your Media, Your Way';
+  static const String appVersion = '1.0.0';
+
+  static const double borderRadius = 12.0;
+  static const double borderRadiusSmall = 8.0;
+  static const double borderRadiusLarge = 16.0;
+
+  static const Duration animFast = Duration(milliseconds: 150);
+  static const Duration animNormal = Duration(milliseconds: 200);
+  static const Duration animMedium = Duration(milliseconds: 250);
+
+  static const int defaultSeekSeconds = 10;
+  static const double defaultPlaybackSpeed = 1.0;
+  static const Duration controlsAutoHideDuration = Duration(seconds: 3);
+
+  static const int navAppClone = 0;
+  static const int navShare = 1;
+  static const int navPlayer = 2;
+  static const int navDownloader = 3;
+  static const int navOtherTools = 4;
+}
