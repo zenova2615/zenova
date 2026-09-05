595b7936d8d2cb96c24f96bb864537e2fd1b5fcc
zenova2615 <shafiahmedmele@gmail.com>
2026-09-05 15:54:09 +0600

    Update video_item.dart

--- lib/data/models/video_item.dart
+++ lib/data/models/video_item.dart
@@ -35,7 +35,9 @@ class VideoItem extends Equatable {
   String get sizeText {
     if (size < 1024) return '$size B';
     if (size < 1024 * 1024) return '${(size / 1024).toStringAsFixed(1)} KB';
-    if (size < 1024 * 1024 * 1024) return '${(size / (1024 * 1024)).toStringAsFixed(1)} MB';
+    if (size < 1024 * 1024 * 1024) {
+      return '${(size / (1024 * 1024)).toStringAsFixed(1)} MB';
+    }
     return '${(size / (1024 * 1024 * 1024)).toStringAsFixed(2)} GB';
   }
 
