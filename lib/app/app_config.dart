9b4dbf6bd4edc4dd0ea12bffd9b2c616457ae9ff
zenova2615 <shafiahmedmele@gmail.com>
2026-09-05 12:18:17 +0600

    Add AppConfig class with application settings
    Define application configuration constants.

---
+++ lib/app/app_config.dart
@@ -0,0 +1,13 @@
+class AppConfig {
+  AppConfig._();
+
+  static const String appName = 'ZENOVA';
+  static const String packageName = 'com.zenova.media';
+  static const bool isDebugMode = true;
+
+  static const bool enableAppClone = true;
+  static const bool enableDownloader = true;
+  static const bool enableShare = true;
+  static const bool enableVault = false;
+  static const bool enableAds = false;
+}
