fd3b9aa55ff8f3f638dd5eb14ddd982c740b1b0a
zenova2615 <shafiahmedmele@gmail.com>
2026-09-06 19:02:26 +0600

    Refactor ZenovaApp to use ConsumerStatefulWidget
    Changed ZenovaApp from ConsumerWidget to ConsumerStatefulWidget to manage state and check for updates on initialization.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,13 +3,30 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/theme/app_theme.dart';
 import '../core/theme/app_colors.dart';
+import '../core/services/update_service.dart';
 import 'routes.dart';
 
-class ZenovaApp extends ConsumerWidget {
+class ZenovaApp extends ConsumerStatefulWidget {
   const ZenovaApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ZenovaApp> createState() => _ZenovaAppState();
+}
+
+class _ZenovaAppState extends ConsumerState<ZenovaApp> {
+  @override
+  void initState() {
+    super.initState();
+    // অ্যাপ চালু হওয়ার একটু পরে আপডেট চেক করবে
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        UpdateService.checkForUpdate(context);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
